51285e2d587023cd102f66e5d9a1562a3dcb49d1
Tenzin Jorden <tenden96@gmail.com>
2022-11-15 00:04:18 -0500

    setting up the modal

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+import {enter, leave, toggle} from 'el-transition'
+
+
+export default class extends Controller {
+  
+  connect() {
+      console.log("dfgd")
+  }
+
+}
